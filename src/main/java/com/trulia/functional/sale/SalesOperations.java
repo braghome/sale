2166d22683746c0863ac4fae1549ff09bc607fc4
mbrar <mbrar@trulia.com>
2016-04-28 15:31:02 -0700

    add reduce example

--- src/main/java/com/trulia/functional/sale/SalesOperations.java
+++ src/main/java/com/trulia/functional/sale/SalesOperations.java
@@ -12,8 +12,23 @@ public class SalesOperations {
 	
 	public List<Sale> collectBrands(final String brand, final List<Sale> sales) {
 		List<Sale> salesOfAPrius = new ArrayList<Sale>();
-		salesOfAPrius.addAll(sales.stream().filter(x -> x.getCar().getBrand().equals(brand))
-				  .collect(Collectors.toList()));	
+		salesOfAPrius.addAll(sales
+								.stream()
+								.filter(x -> x.getCar().getBrand().equals(brand))
+				     .collect(Collectors.toList()));	
 		return salesOfAPrius;
+	}
+	
+	public List<Sale> youngestPerson(final List<Person> persons, final List<Sale> sales) {
+		Person youngest = persons.stream()
+								 .reduce((x, y) -> 
+								 			x.getAge() < y.getAge() ? x : y)
+					 			 .orElse(new Person("the one", "one only", 1, true));
+		List<Sale> buys = new ArrayList<Sale>();
+		buys.addAll(sales.stream()
+				 		 .filter(x -> x.equals(youngest))
+				 		 .collect(Collectors.toList())
+		 		   );
+		return buys;
 	}
 }
